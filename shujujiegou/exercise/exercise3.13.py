cfc17a7140272e4e4d1801bc0ad8871ddd7902ed
LAPTOP-H5VTCBPL\30337 <3033175900@qq.com>
2021-12-10 18:17:40 +0800

    master

--- shujujiegou/exercise/exercise3.13.py
+++ shujujiegou/exercise/exercise3.13.py
@@ -3,3 +3,16 @@
 # @Author : wzq
 # @File : exercise3.13.py
 # @Software : PyCharm
+from LinkQueue import LinkQueue
+qu = LinkQueue()
+def Jsequence(n,m):
+    for i in range(1,n+1) :
+        qu.push()
+    for i in range(1,n+1):
+        j = 1
+        while j <= m-1 :
+            qu.push(qu.pop())
+            j += 1
+        x = qu.pop()
+        print(x,end = '')
+print()
